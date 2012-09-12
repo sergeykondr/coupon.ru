ecb5a185142341d3140093468366094f8cdaf102
Alexey Sharov <alexey.sharov@mebelrama.ru>
2012-09-12 19:39:24 +0400

    add baseUrl

--- protected/views/layouts/admin/login.php
+++ protected/views/layouts/admin/login.php
@@ -6,22 +6,24 @@
     <title>Yii CMS Admin Panel</title>
 
     <?
+    $base = Yii::app()->baseUrl;
+
     $cs = Yii::app()->clientScript;
     $cs->registerCoreScript('jquery');
     $cs->registerCoreScript('jquery.ui');
     Yii::app()->bootstrap->registerScripts();
-    $cs->registerCssFile('/css/admin/layout.css');
-    $cs->registerCssFile('/css/admin/form.css');
-    $cs->registerCssFile('/css/admin/extend.css');
-    $cs->registerScriptFile('/js/admin/hideshow.js');
-    $cs->registerScriptFile('/js/admin/jquery.tablesorter.min.js');
-    $cs->registerScriptFile('/js/admin/jquery.equalHeight.js');
-    $cs->registerScriptFile('/js/admin/main.js');
+    $cs->registerCssFile($base.'/css/admin/layout.css');
+    $cs->registerCssFile($base.'/css/admin/form.css');
+    $cs->registerCssFile($base.'/css/admin/extend.css');
+    $cs->registerScriptFile($base.'/js/admin/hideshow.js');
+    $cs->registerScriptFile($base.'/js/admin/jquery.tablesorter.min.js');
+    $cs->registerScriptFile($base.'/js/admin/jquery.equalHeight.js');
+    $cs->registerScriptFile($base.'/js/admin/main.js');
     ?>
 
     <!--[if lt IE 9]>
-    <link rel="stylesheet" href="/css/admin/ie.css" type="text/css" media="screen"/>
-    <script src="/js/admin/html5.js"></script>
+    <link rel="stylesheet" href="<?= $base ?>/css/admin/ie.css" type="text/css" media="screen"/>
+    <script src="<?= $base ?>/js/admin/html5.js"></script>
     <![endif]-->
 
     <style type="text/css">
