cb8b48edd20afaed82282268b295ae3ec3ee4800
Sergey Kondratiev <sergeykondr@gmail.com>
2012-10-09 20:10:17 +0400

    создание акции

---
+++ protected/modules/content/forms/DiscountForm.php
@@ -0,0 +1,60 @@
+<?
+return array(
+    'enctype'              => 'multipart/form-data',
+    'activeForm'           => array(
+        'id'            => 'page-form',
+        'clientOptions' => array('validateOnSubmit' => true),
+    ),
+    'elements'             => array(
+        'title'    => array(
+            'type' => 'text'
+        ),
+        /*
+        'category_id'    => array(
+            'type' => 'text'
+        ),
+        */
+        'category_id' => array(
+            'type'  => 'dropdownlist',
+            'items' => CHtml::listData(Category::model()->findAll(), 'id', 'name'),
+            'empty' => 'не выбран'
+        ),
+
+        'caption'    => array(
+            'type' => 'text'
+        ),
+        'text'    => array(
+            'type' => 'text'
+        ),
+//        'url' => array(
+//            'type'   => 'alias',
+//            'source' => 'title'
+//        ),
+    /*
+        'files'    => array(
+            'type'      => 'uploader_modal',
+            'data_type' => 'image',
+            'title'     => 'Файлы'
+        ),
+
+
+        'status'   => array(
+            'type'  => 'dropdownlist',
+            'items' => Page::$status_options
+        ),
+        'tags'     => array(
+            'type'  => 'tags',
+            'label' => 'Теги'
+        ),
+    */
+        'text'     => array(
+            'type' => 'editor'
+        ),
+    ),
+    'buttons'              => array(
+        'submit' => array(
+            'type'  => 'submit',
+            'value' => t('сохранить')
+        )
+    )
+);
