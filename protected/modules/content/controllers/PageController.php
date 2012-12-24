fe708c65c06cd7c91f750806c7cf196dcfa9cb38
Sergey Kondratiev <sergeykondr@gmail.com>
2012-12-24 18:08:48 +0400

    подправил меню, удалил комментарии

--- protected/modules/content/controllers/PageController.php
+++ protected/modules/content/controllers/PageController.php
@@ -40,76 +40,21 @@ class PageController extends Controller
         //записываем первое меню - 'Все' (его нет в БД)
         $menu[] = array(
             'label' => t('Все'),
-            'url'   => array('discount/category/all')
+            'url'   => array('/discount/discount/index')
         );
         //записываем всё остальное меню
         //первая часть url для страниц категорий
-        $urlPart = 'discount/category/';
+        $urlPart = '/discount/discount/category';
         foreach ($categories as $name)
         {
             //обращаемся к модели категорий по id, узнаем кол-во акций
             $count = Category::model()->with('discountCount')->findByPk($name->id);
             $menu[] = array(
                 'label' => t($name->name.' ('.$count->discountCount.')'),
-                'url'   => array($urlPart.$name->url)
+                'url'   => array($urlPart, 'cat' => $name->url )
             );
         }
         return $menu;
-
-        /*return array(
-            array(
-                'label' => t('Все'),
-                'url'   => array('discount/category/all')
-            ),
-            array(
-                'label' => t('Красота'),
-                'url'   => array('discount/category/beauty')
-            ),
-            array(
-                'label' => t('Здоровье'),
-                'url'   => array('discount/category/health')
-            ),
-            array(
-                'label' => t('Еда'),
-                'url'   => array('discount/category/food')
-            ),
-            array(
-                'label' => t('Развлечения'),
-                'url'   => array('discount/category/entertainment')
-            ),
-            array(
-                'label' => t('Отдых'),
-                'url'   => array('discount/category/rest')
-            ),
-            array(
-                'label' => t('Товары'),
-                'url'   => array('discount/category/beauty')
-            ),
-            array(
-                'label' => t('Фото'),
-                'url'   => array('discount/category/photo')
-            ),
-            array(
-                'label' => t('Обучение'),
-                'url'   => array('discount/category/education'),
-                'active'=>false
-            ),
-            array(
-                'label' => t('Авто'),
-                'url'   => array('discount/category/auto')
-            ),
-            array(
-                'label' => t('Прочее'),
-                'url'   => array('discount/category/other')
-            ),
-            array(
-                'label'   => Yii::app()->user->isGuest ?: t('Ваши') . '(' . Page::model()->count('user_id = ' . Yii::app()->user->id) . ')',
-                'url'     => array('/page/user/' . Yii::app()->user->id),
-                'visible' => !Yii::app()->user->isGuest
-            )
-        );*/
-
-
     }
 
 
