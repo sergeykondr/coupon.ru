59195d043e0d804596f43c7fbc39ad9c6630c9c8
Sergey Kondratiev <sergeykondr@gmail.com>
2013-01-16 01:40:20 +0400

    #6 div, css, overflow:hidden

--- protected/modules/discount/views/discount/_view.php
+++ protected/modules/discount/views/discount/_view.php
@@ -1,45 +1,50 @@
 <?= (($index % 3)==0 ) ? '<div class="row-fluid">' : '' ?>
     <div class="span4 well">
+        <div class="img_category">
         <?
         $i=0; //счетчик для первой картинки
         $imghtml=''; //сюда пишем адрес первой картинки
 
         //если акция наша - отображаем первую картинку из галереи
         //если импортированная - то ссылается на сайт
-        if ($data->our)
-        {
-            foreach($data->gallery as $gal)
+
+            if ($data->our)
             {
-                $imghtml = CHtml::image($gal->getHref(),'',array('class'=>'img-rounded'));
-                break; //т.к. берем первую картинку
+                foreach($data->gallery as $gal)
+                {
+                    $imghtml = CHtml::image($gal->getHref(),'',array('class'=>'img-rounded'));
+                    break; //т.к. берем первую картинку
+                }
             }
-        }
-        else
-        {
+            else
+            {
 
-            $imghtml = CHtml::image($data->xml[0]->getHref(),'',array('class'=>'img-rounded'));
-        }
+                $imghtml = CHtml::image($data->xml[0]->getHref(),'',array('class'=>'img-rounded'));
+            }
 
-        echo CHtml::link($imghtml, $data->href, array('class' => 'page-title'));
-        ?>
-        <br>
-        <!--заголовок-->
-        <?= CHtml::link(CHtml::encode($data->name), '/discount/'.$data->id, array('class' => 'page-title')); ?>
-        <br>
-        <p><small>Купон действует до <?= Yii::app()->dateFormatter->format('d MMMM yyyy', $data->endvalid); ?></small></p>
-        Метро
-        <br>
-        Скидка <? echo CHtml::encode($data->discount); ?>% за <? echo CHtml::encode($data->pricecoupon); ?> р.
-        <br>
-        <div class="row-fluid">
-            <div class="span6">
-                <center>Осталось:  <?=$data->expires('short');?></center>
+            echo CHtml::link($imghtml, $data->href, array('class' => 'page-title'));
+            ?>
             </div>
-            <div class="span6">
-                <center>Купили:  <? echo $data->cheat() + $data->numbers_buy;  ?></center>
-                <!-- <br clear="all"/> -->
-            </div>
+        <br>
+        <div class="body_category">
+            <!--заголовок-->
+            <?= CHtml::link(CHtml::encode($data->name), '/discount/'.$data->id, array('class' => 'page-title')); ?>
         </div>
+            <p><small>Купон действует до <?= Yii::app()->dateFormatter->format('d MMMM yyyy', $data->endvalid); ?></small></p>
+            Метро
+            <br>
+            Скидка <? echo CHtml::encode($data->discount); ?>% за <? echo CHtml::encode($data->pricecoupon); ?> р.
+            <br>
+            <div class="row-fluid">
+                <div class="span6">
+                    <center>Осталось:  <?=$data->expires('short');?></center>
+                </div>
+                <div class="span6">
+                    <center>Купили:  <? echo $data->cheat() + $data->numbers_buy;  ?></center>
+                    <!-- <br clear="all"/> -->
+                </div>
+            </div>
+
     </div>
 <?= (($index % 3)==2 ) ? '</div>' : '' ?>
 
